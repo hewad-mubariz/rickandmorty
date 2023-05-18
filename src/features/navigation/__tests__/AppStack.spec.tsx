b11e1cb8dcde11e1363a72d2b712c6531ce7c032
hewad-mubariz <hewadmubariz@gmail.com>
2023-05-19 01:12:40 +0200

    Added test case for AppStack

--- src/features/navigation/__tests__/AppStack.spec.tsx
+++ src/features/navigation/__tests__/AppStack.spec.tsx
@@ -1,6 +1,11 @@
 import React from "react";
-import { cleanup } from "@testing-library/react-native";
-
+import { cleanup, render } from "@testing-library/react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { AppStack } from "../AppStack";
+import { MockedProvider } from "@apollo/client/testing";
+import { allCharactersMock } from "../../../../__mocks__/query-mocks/characterQueriesMocks";
+import { renderWithProvider } from "../../../utils/test-utils";
+allCharactersMock;
 // We are using React Native >= 0.64
 jest.mock("react-native/Libraries/Animated/NativeAnimatedHelper");
 
@@ -9,6 +14,7 @@ describe("AppStack", () => {
     cleanup();
   });
   it("Checks if navigation renders correct", async () => {
-    expect(true).toBe(true);
+    //todo remove warning for act
+    renderWithProvider(<AppStack />, []);
   });
 });
